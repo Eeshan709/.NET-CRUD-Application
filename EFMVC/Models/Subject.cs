6b46142cff084609d8048202bfb3c39a8f2ef5e5
Eeshan709 <eeshanpoornima97@gmail.com>
2022-06-29 15:08:03 +0530

    Add project files.

---
+++ EFMVC/Models/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFMVC.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public string SubjectTerm { get; set; }
+        public string SubjectCredets { get; set; }
+    }
+}
